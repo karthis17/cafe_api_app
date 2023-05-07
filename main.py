1a8f4ae88f56f36a4ae3f44f0f05f7d21a54ec9a
karthis17 <karthirs602@gmail.com>
2023-05-07 12:40:35 +0530

    sql update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:htmH2VfF2UD7fEdDpItyv5tmRougniRQ@dpg-ch40h7seooglumagtc4g-a.oregon-postgres.render.com/blog_ftc6"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -74,14 +75,14 @@ def get_all_cafe():
                                    "coffee_price": cafe.coffee_price} for cafe in cafes})
 
 
-@app.route("/search")
+@app.route("/search", methods=["GET", "POST"])
 def get_cafe_at_location():
-    query_location = request.form.get("loc")
-    cafes = db.session.query(Cafe).filter_by(location=query_location).first()
+    query_location = request.args.get("loc")
+    cafes = db.session.query(Cafe).filter(Cafe.location.ilike(f'%{query_location}%')).all()
     if cafes:
-        return jsonify(cafe=cafes.to_dict())
+        return jsonify(cafe={cafe.id: cafe.to_dict() for cafe in cafes})
     else:
-        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+        return jsonify(error={"Not Found": "Sorry, we don't know any cafe at that location."})
 
 
 # HTTP POST - Create Record
@@ -93,16 +94,19 @@ def add_cafe():
         map_url=request.args.get("map_url"),
         img_url=request.args.get("img_url"),
         location=request.args.get("loc"),
-        has_sockets=request.args.get("sockets"),
-        has_toilet=request.args.get("toilet"),
-        has_wifi=request.args.get("wifi"),
-        can_take_calls=request.args.get("calls"),
+        has_sockets=bool(int(request.args.get("sockets"))),
+        has_toilet=bool(int(request.args.get("toilet"))),
+        has_wifi=bool(int(request.args.get("wifi"))),
+        can_take_calls=bool(int(request.args.get("calls"))),
         seats=request.args.get("seats"),
         coffee_price=request.args.get("coffee_price"),
     )
-    db.session.add(new_cafe)
-    db.session.commit()
-    return jsonify(Success="New cafe details are successfully added! ")
+    try:
+        db.session.add(new_cafe)
+        db.session.commit()
+        return jsonify(Success="New cafe details are successfully added! ")
+    except:
+        return jsonify(Failed="Adding process not well done please check the data")
 
 
 # HTTP PUT/PATCH - Update Record
@@ -120,10 +124,10 @@ def update_cafe(id_to_update):
 
 
 # HTTP DELETE - Delete Record
-@app.route("/delete-report/<_id_>", methods=["POST"])
+@app.route("/delete-report/<_id_>", methods=["POST", "GET"])
 def report_to_delete(_id_):
     cafe = db.session.query(Cafe).get(_id_)
-    api_secret_key = "!@#$%^&*()"
+    api_secret_key = "qwertyuiopasdfghjkl"
     if cafe:
         if request.args.get("secret_key") == api_secret_key:
             db.session.delete(cafe)
